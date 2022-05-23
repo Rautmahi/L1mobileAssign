fdb062b408b19a037b4955c5ab188b191ad71d4a
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2022-05-23 22:35:28 +0530

    mobiledata

---
+++ src/component/Mobile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const MobileOs =()=>{
+
+    return (
+    <div>
+        <h1> Mobile Manufacturing System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>I-Phone</li>
+        <li>Windows-Phone</li>
+
+
+        <h1>Mobile Manufacturing</h1>
+        <li id="sam">Samsung</li>
+        <li id="sam">HTC</li>
+        <li>Micromax</li>
+        <li id="app"  >Apple</li>
+
+    </div>
+    )
+}
+export default MobileOs
